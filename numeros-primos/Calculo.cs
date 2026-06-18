e05821390f94672280c4f5423a89310e079706f2
MarcosViniciusRychlewski <vinirychlewski@gmail.com>
2026-06-18 17:21:58 -0300

    primeiro commit

---
+++ numeros-primos/Calculo.cs
@@ -0,0 +1,38 @@
+﻿namespace numeros_primos
+{
+    internal class Calculo
+    {
+        internal static int[] calcularNumerosPrimos(int numInicial, int numFinal)
+        {
+            var numerosPrimos = new List<int>();
+
+            //for para iterar do número inicial ao número final
+            for (var i = numInicial; i <= numFinal; i++)
+            {
+                //verificar se o número é primo
+                if (isPrimo(i))
+                {
+                    numerosPrimos.Add(i);
+                }
+            }
+
+            return numerosPrimos.ToArray();
+        }
+
+        static bool isPrimo(int num)
+        {
+            if (num < 2) return false;
+
+            //verificar se o número é divisível por algum número entre 2 e a raiz quadrada do número
+            for (var i = 2; i * i <= num; i++)
+            {
+                //se o número for divisível por algum número, ele não é primo
+                if (num % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ numeros-primos/Program.cs
@@ -0,0 +1,8 @@
+﻿using numeros_primos;
+
+const int numInicial = 0;
+var numFinal = 1000;
+
+var numerosPrimos = Calculo.calcularNumerosPrimos(numInicial, numFinal);
+Console.WriteLine("Números primos entre " + numInicial + " e " + numFinal + ": " + string.Join(", ", numerosPrimos));
+Console.WriteLine("Total de números primos encontrados: " + numerosPrimos.Length);
